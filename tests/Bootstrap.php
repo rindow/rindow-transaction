d67c8b30dd5a1e68503ed38d572d9cb35fea0995
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-04-02 14:57:59 +0900

    for public packagist

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -5,3 +5,6 @@ include 'init_autoloader.php';
 if(!class_exists('PHPUnit\Framework\TestCase')) {
     include __DIR__.'/travis/patch55.php';
 }
+if(getenv('TRAVIS_SKIP_TEST')) {
+    define('TRAVIS_SKIP_TEST', true);
+}
